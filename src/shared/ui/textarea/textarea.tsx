9224bc9d7b8644839b867c750b46c142c4e1d6a3
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-02-22 19:36:30 +0300

    feat/ZHE-15: add textarea default component

---
+++ src/shared/ui/textarea/textarea.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { useRef, useEffect } from 'react'
+import { typeTextareaProps } from '@/shared/ui/textarea/types'
+import classes from './styles.module.scss'
+import cn from 'classnames'
+
+export const Textarea: React.FC<typeTextareaProps> = ({
+  className,
+  label,
+  value,
+  setValue,
+  required
+}) => {
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null)
+
+  const autoResize = (): void => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto'
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`
+    }
+  }
+
+  useEffect(() => {
+    autoResize()
+  }, [])
+
+  useEffect(() => {
+    const handleResize = (): void => autoResize()
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <div className={cn(className, classes.textarea__container)}>
+      <label className={cn(className, classes.textarea__label)} htmlFor='textarea'>
+        {label}
+        {required && <span className={cn(className, classes.textarea__span)}>*</span>}
+      </label>
+      <textarea
+        ref={textareaRef}
+        className={cn(className, classes.textarea)}
+        id='textarea'
+        value={value}
+        autoComplete='off'
+        onChange={(e) => {
+          setValue(e.target.value)
+          autoResize()
+        }}
+        required={required}
+      />
+    </div>
+  )
+}
