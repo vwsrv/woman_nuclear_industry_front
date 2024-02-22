87c71113b45165999fc60f950069721339d644b9
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-02-22 20:42:49 +0300

    Revert "feat/ZHE-15: add textarea default component"
    This reverts commit 55a6b60be0947f4cdb45da3ca5fa1ea374585bd8.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './global.scss';
+import './globals.css';
 import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
--- src/shared/ui/textarea/types.ts
+++
@@ -1,8 +0,0 @@
-import { TextareaHTMLAttributes, DetailedHTMLProps, SetStateAction, Dispatch } from 'react'
-
-export interface typeTextareaProps extends DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
-  label: string
-  value: string
-  setValue: Dispatch<SetStateAction<string>>
-  required: boolean
-}
