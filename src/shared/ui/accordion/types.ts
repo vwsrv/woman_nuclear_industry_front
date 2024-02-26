aa3ae03a01c1cad2c5f743190e430a16fa2c1e4c
Alexey Ushakov <alexeyushakov16@gmail.com>
2024-02-26 09:52:19 +0300

    feature: animation for opening/fix: types and state for accordion props

--- src/shared/ui/accordion/types.ts
+++ src/shared/ui/accordion/types.ts
@@ -1,16 +1,21 @@
 import { inter } from '@/app/fonts/fonts';
 import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 
-export interface AccordionProps
+export interface typeAccordionItem
   extends DetailedHTMLProps<
     ButtonHTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement
   > {
   children: React.ReactNode | JSX.Element;
   title: string;
+}
+
+export interface typeAccordionProps {
+  props: typeAccordionItem[];
+}
+
+export interface typeAccordionItemWithState extends typeAccordionItem {
   isOpen: boolean;
 }
 
-export type AccordionPropsType = {
-  props: AccordionProps[];
-};
+export type typeAccordionItemWithStateArray = typeAccordionItemWithState[];
