1d36bf33b6c9125b23b25c0656ee0337a45c4930
Bl Bltich <vissar2016@yandex.ru>
2024-02-26 16:06:15 +0300

    refactor: added test components

--- src/pages/test-page/test-page.tsx
+++ src/pages/test-page/test-page.tsx
@@ -1,7 +1,14 @@
 'use client';
 
 import { Button } from '@/shared/ui/button';
+import { SelectBox } from '@/shared/ui/selectbox';
 import React from 'react';
+
+const selectBoxExample = [
+  { value: '1', label: 'Физика' },
+  { value: '2', label: 'Биология' },
+  { value: '3', label: 'Социология' }
+];
 
 export const TestPage: React.FC = () => {
   return (
@@ -15,6 +22,11 @@ export const TestPage: React.FC = () => {
         >
           Test button
         </Button>
+        <SelectBox
+          disabled={false}
+          options={selectBoxExample}
+          className="test-class"
+        ></SelectBox>
       </div>
     </div>
   );
