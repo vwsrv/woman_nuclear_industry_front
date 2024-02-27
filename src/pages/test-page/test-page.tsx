d39c28144dfcab20a1d25343f7704e103587f816
Bl Bltich <vissar2016@yandex.ru>
2024-02-28 00:44:47 +0300

    refactor: tests components added

--- src/pages/test-page/test-page.tsx
+++ src/pages/test-page/test-page.tsx
@@ -5,28 +5,23 @@ import { SelectBox } from '@/shared/ui/selectbox';
 import React from 'react';
 
 const selectBoxExample = [
-  { value: '1', label: 'Физика' },
-  { value: '2', label: 'Биология' },
-  { value: '3', label: 'Социология' }
+  { value: '0', label: 'Биология', link: '#' },
+  { value: '1', label: 'Химия', link: '#' },
+  { value: '2', label: 'Физика', link: '#' },
+  { value: '3', label: 'СПЕЦНОМИНАЦИЯ: ЖЕНЩИНЫ-УЧЕНЫЕ АРКТИКИ', link: '#' }
 ];
+
+const onClick = () => {
+  console.log('Button is clicked');
+};
 
 export const TestPage: React.FC = () => {
   return (
     <div>
       <h1>Test page</h1>
       <div>
-        <Button
-          disabled={false}
-          className="test-class"
-          onClick={() => console.log('Test click')}
-        >
-          Test button
-        </Button>
-        <SelectBox
-          disabled={false}
-          options={selectBoxExample}
-          className="test-class"
-        ></SelectBox>
+        <SelectBox options={selectBoxExample} name="Выберите категорию" />
+        <Button onClick={onClick}>Добавить</Button>
       </div>
     </div>
   );
