2a265bb7d4c01b11909c89b11e113d0be2514818
JackSheepe <teterkinvklol@mail.ru>
2024-03-07 01:02:33 +0700

    fix: test page returned to initial state

--- src/pages/test-page/test-page.tsx
+++ src/pages/test-page/test-page.tsx
@@ -1,32 +1,19 @@
 'use client';
 
 import { Button } from '@/shared/ui/button';
-import { Highlight } from '@/shared/ui/highlight';
 import React from 'react';
-import bgimage from '@/shared/images/for-highlight/bgimage.svg';
-
 export const TestPage: React.FC = () => {
   return (
     <div>
       <h1>Test page</h1>
       <div>
-        <Highlight title="Поддержка проектов" bgImage={bgimage}>
-          <>
-            <p>
-              Хотите стать участником самого крупного женского научного
-              сообщества в России, партнером или инфопартнером, волонтером,
-              колумнистом блога, меценатом? Оставьте заявку ниже или поддержите
-              проект!
-            </p>
-            <Button
-              disabled={false}
-              className="test-class"
-              onClick={() => console.log('Test click')}
-            >
-              Test button
-            </Button>
-          </>
-        </Highlight>
+        <Button
+          disabled={false}
+          className="test-class"
+          onClick={() => console.log('Test click')}
+        >
+          Test button
+        </Button>
       </div>
     </div>
   );
