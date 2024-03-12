b425dd98136093926a28d9f49e679dceb82d1d95
Bl Bltich <vissar2016@yandex.ru>
2024-03-12 23:40:39 +0300

    refactor: test component added

--- src/pages/test-page/test-page.tsx
+++ src/pages/test-page/test-page.tsx
@@ -1,15 +1,35 @@
 'use client';
 
 import { Button } from '@/shared/ui/button';
-import React from 'react';
+import React, { useState } from 'react';
+import { Popup } from '@/shared/ui/popup';
 export const TestPage: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onPopupOpen = () => {
+    setIsOpen(true);
+  };
+
+  const onPopupClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div>
       <h1>Test page</h1>
       <div>
-        <Button disabled={false} className="test-class">
+        <Button disabled={false} className="test-class" onClick={onPopupOpen}>
           Test button
         </Button>
+        <Popup
+          isOpen={isOpen}
+          onClose={onPopupClose}
+          isCloseByOverlay={isOpen}
+          title="Профиль"
+        >
+          <div>Я первый контейнер с картинками</div>
+          <div>Я второй контейнер</div>
+        </Popup>
       </div>
     </div>
   );
