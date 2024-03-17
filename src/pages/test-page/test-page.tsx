c0ae38d186f6bb5b789eb5d04a3313d408cea618
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-03-18 02:00:33 +0500

    fix: resetting changes to src/pages/test-page/test-page.tsx

--- src/pages/test-page/test-page.tsx
+++ src/pages/test-page/test-page.tsx
@@ -1,25 +1,12 @@
 'use client';
 
 import { Button } from '@/shared/ui/button';
-import { Input } from '@/shared/ui/input';
 import React from 'react';
 export const TestPage: React.FC = () => {
-  const [value, setValue] = React.useState<string>('sdfsdf11');
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setValue(e.target.value);
-  };
-
-  const [value1, setValue1] = React.useState<string>('');
-
-  const handleChange1 = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setValue1(e.target.value);
-  };
-
   return (
     <div>
       <h1>Test page</h1>
-      {/* <div>
+      <div>
         <Button
           disabled={false}
           className="test-class"
@@ -27,41 +14,6 @@ export const TestPage: React.FC = () => {
         >
           Test button
         </Button>
-      </div> */}
-
-      <div
-        style={{
-          maxWidth: '440px',
-          padding: '20px',
-          display: 'flex',
-          flexDirection: 'column'
-          // backgroundColor: '#f99'
-          // justifyContent: 'center',
-        }}
-      >
-        <div style={{ margin: '0 0 20px' }}>
-          <Input
-            label="Ваше имя и фамилия:"
-            required={false}
-            value={value1}
-            onChange={handleChange1}
-            name="name"
-            type="text"
-          />
-        </div>
-
-        <div style={{ margin: '0 0 20px' }}>
-          <Input
-            label="Обязательное поле:"
-            required={true}
-            value={value}
-            onChange={handleChange}
-            name="requeridField"
-            type="text"
-            // className="test-class"
-            // disabled={true}
-          />
-        </div>
       </div>
     </div>
   );
