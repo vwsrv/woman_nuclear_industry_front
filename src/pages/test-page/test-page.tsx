1e2dd00571431d730f1393ff9864730fca6a7476
JackSheepe <teterkinvklol@mail.ru>
2024-03-21 01:22:00 +0700

    fix: deleted mockup

--- src/pages/test-page/test-page.tsx
+++ src/pages/test-page/test-page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Button } from '@/shared/ui/button';
-import { Mission } from '@/shared/ui/mission';
 import React from 'react';
 export const TestPage: React.FC = () => {
   return (
@@ -16,16 +15,6 @@ export const TestPage: React.FC = () => {
           Test button
         </Button>
       </div>
-      <Mission
-        title="Миссия"
-        text="Формирование профессионального отраслевого женского сообщества и развитие международного сотрудничества в области использования атомной энергии, укрепление горизонтального взаимодействия и эффективной коммуникации между работниками из различных регионов, популяризация научно-технических профессий среди женщин и их всестороннее развитие."
-        counters={[
-          { value: '140+', label: 'Реализованных проектов' },
-          { value: '1500+', label: 'Участниц из 37 субъектов России' },
-          { value: '38', label: 'Инициативных групп' },
-          { value: '17', label: 'Зарубежных стран' }
-        ]}
-      ></Mission>
     </div>
   );
 };
