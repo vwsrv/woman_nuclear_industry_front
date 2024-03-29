188db5ba7767e29f35cdf37b95f3892861ce8c5f
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-03-30 03:14:00 +0500

    init: start ProfileForm task

---
+++ src/shared/ui/profile-form/profile-form.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { typeProfileFormProps } from '@/shared/ui/profile-form/types';
+import { FC } from 'react';
+import classes from './styles.module.scss';
+import cn from 'classnames';
+
+export const ProfileForm: FC<typeProfileFormProps> = props => {
+  const {
+    ...otherProps
+  } = props;
+
+  return (
+    <>
+    </>
+  );
+};
---
+++ src/shared/ui/profile-form/types.ts
@@ -0,0 +1,13 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export interface typeProfileFormProps
+  extends DetailedHTMLProps<
+    HTMLAttributes<HTMLDivElement>, 
+    HTMLDivElement
+  > {
+  // label?: string;
+  // name?: string;
+  // value: string;
+  // handleInputChange: (newValue: string) => void;
+  // type?: 'text' | 'email' | 'tel' | 'password' | 'url' | 'number' | 'date';
+}
