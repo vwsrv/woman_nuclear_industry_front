937f1acfa9bba8a6dd28cc649175b7a9f4485dbe
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-04-11 19:58:44 +0300

    refactor: backup test-page

--- src/pages/test-page/test-page.tsx
+++ src/pages/test-page/test-page.tsx
@@ -1,39 +1,21 @@
 'use client';
 
-import { About } from '@/shared/ui/about';
 import { Button } from '@/shared/ui/button';
-import { SignupForm } from '@/shared/ui/signup-form';
-import { usePathname } from 'next/navigation';
-import React, { SetStateAction, useState } from 'react';
-import { useForm, FormProvider } from 'react-hook-form';
+import React from 'react';
 
 export const TestPage: React.FC = () => {
-  const methods = useForm();
-
-  const pathname = usePathname();
-  const [consent, setConsent] = useState(false);
-
-  const toggleConsent = (): void => setConsent(!consent);
-
-  const onSubmit = (data: any) => console.log(data);
-
   return (
     <div>
       <h1>Test page</h1>
       <div>
-        <Button disabled={false} className="test-class">
+        <Button
+          disabled={false}
+          className="test-class"
+          onClick={() => console.log('Test click')}
+        >
           Test button
         </Button>
       </div>
-      <FormProvider {...methods}>
-        <SignupForm
-          pathname={pathname}
-          consent={consent}
-          toggleConsent={toggleConsent}
-          onSubmit={methods.handleSubmit(onSubmit)}
-          className="signup-form"
-        />
-      </FormProvider>
     </div>
   );
 };
