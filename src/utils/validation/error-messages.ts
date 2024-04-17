f31ae33a90301bff73e176de2d3b0862aeaaff95
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-04-17 14:59:19 +0300

    refactor: refactor date message

--- src/utils/validation/error-messages.ts
+++ src/utils/validation/error-messages.ts
@@ -2,7 +2,7 @@ const EMAIL_MESSAGE = 'Введите почту в формате email@email.r
 const FIRSTNAME_MESSAGE = 'Имя дожно состоять минимум из 2-х символов.';
 const LASTNAME_MESSAGE = 'Фамилия дожна состоять минимум из 2-х символов.';
 const PHONE_MESSAGE = 'Номер телефона должен начинаться с +7.';
-const DATE_MESSAGE = 'Пожалуйста введите дату в формате дд/мм/ггг.';
+const DATE_MESSAGE = 'Пожалуйста введите дату в формате дд/мм/гггг.';
 const PASSWORD_MESSAGE = 'Пароль должен состоять не менее чем из 8 символов.';
 const REQUIRED_MESSAGE = 'Поле должно быть обязательно заполнено.';
 
