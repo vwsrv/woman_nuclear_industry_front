c57b4277b258117f2e7bf329e7d1004d4bda4a19
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-04-24 05:33:37 +0500

    fix: saving intermediate version

--- src/shared/ui/profile-form/types.ts
+++ src/shared/ui/profile-form/types.ts
@@ -2,6 +2,12 @@ import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
 export interface typeProfileFormProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  userInfo: {
+    logo?: string;
+    name: string;
+    address: string;
+    dateOfBirth: Date;
+  };
   // label?: string;
   // name?: string;
   // value: string;
