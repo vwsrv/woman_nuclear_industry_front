9a05e339d18d829f75c259fbed454499a6ffb803
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-04-25 16:00:13 +0300

    refactor: refactor layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import cn from 'classnames';
 import classes from './styles.module.scss';
-import { ReactNode } from 'react';
+const AuthLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return <div className={cn(classes.authLayout)}>{children}</div>;
+};
 
-export const AuthLayout = ({ children }: { children: ReactNode }) => (
-  <div className={cn(classes.authLayout)}>{children}</div>
-);
+export default AuthLayout;
